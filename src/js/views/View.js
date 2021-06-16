473e0ca131dbac4013d7da01b618b8e02f024092
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-17 01:55:09 +0530

    dist build added

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -9,8 +9,7 @@ export default class View {
      * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
      * @returns {undefined | string} A markup string is returned if render=false
      * @this {Object} View instance
-     * @author Jonas Schmedtmann
-     * @todo Finish implementation
+     * @author Jeeva Kalaiselvam
      */
     render(data, render = true) {
         if (!data || (Array.isArray(data) && data.length === 0))
